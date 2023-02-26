50949a8ee9ab1fe230d7990702033ae19dd9f7cb
Amir Nassar <ameer_n11@hotmail.com>
2023-02-26 15:48:03 +0200

    the Game Project is done

--- MainScores.py
+++ MainScores.py
@@ -1,3 +1,5 @@
+from builtins import Exception
+
 from flask import *
 from Utils import SCORES_FILE_NAME
 import os
@@ -6,12 +8,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     os.system('python MainGame.py')
-    f = open(SCORES_FILE_NAME,"r")
-    numScoure = f.read()
-    numScoure = int(numScoure)
-    f.close()
-
-    return(f'<html><head><title>Scores Game</title></head><body><h1>The score is <div id="score">{numScoure}</div></h1></body></html>')
-
+    try:
+        f = open(SCORES_FILE_NAME,"r")
+        numScoure = f.read()
+        numScoure = int(numScoure)
+        f.close()
+        return (
+            f'<html><head><title>Scores Game</title></head><body><h1>The score is <div id="score">{numScoure}</div></h1></body></html>')
+    except Exception as e:
+        return(f'<html><head><title>Scores Game</title></head><body><h1>The Exception is:  <div id="score">{type(e)}</div></h1></body></html>')
+    
 if __name__ == "__main__":
     app.run(debug=True, port=9000)
